75db62586677658e24cd2639c4b77ce35f8f260f
Tanu9569 <tanusharma9569@gmail.com>
2025-04-09 13:52:42 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ import { Button } from "./Button";
 export const Footer = () => {
   return (
     <footer className="flex flex-row justify-center items-center gap-4 bg-[#121212] text-[#FFDB70] py-4">
-      <p className="text-sm">Made with ❤️ by Sangeetha Choudhary</p>
+      <p className="text-sm">Made by Tanu Sharma</p>
       <Button>
-        <a href="https://github.com/3Sangeetha3/Zynetic" target="_blank" rel="noopener noreferrer">
+        <a href="https://github.com/Tanu9569/Weather-Dashboard-App" target="_blank" rel="noopener noreferrer">
             GitHub
         </a>
       </Button>
